9cab338325cf3bdaa156d7a79d7b950fde2c17b1
javeed9890 <khwajajaveed@gmail.com>
2025-04-14 16:32:02 +0530

    Update ec2-creation.tf

--- terraformscripts/ec2-creation.tf
+++ terraformscripts/ec2-creation.tf
@@ -2,12 +2,13 @@ provider "aws"{
 region = "ap-south-1"
 }
 
-resource "aws_instance" "AWSEC2Instance"{
-   ami = "ami-0d682f26195e9ec0f"
-   instance_type = "t2.micro"
-   key_name = "devopsjan2025"
-   security_groups = ["launch-wizard-1"]
-   tags={
-   Name = "terraformserver"
-   }
+resources "aws_instance" "AWSINSTANCE"{
+ami = "ami-002f6e91abff6eb96"
+instance_type = "t2.micro"
+key_name = "javeed.pem"
+security_groups = ["default"]
+tags={
+Name = "terraformserver"
 }
+}
+
