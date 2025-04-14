f0a96264a8ba1bbe816727ce3859cd63b5ea24bb
javeed9890 <khwajajaveed@gmail.com>
2025-04-14 16:54:29 +0530

    Update ec2-creation.tf

--- terraformscripts/ec2-creation.tf
+++ terraformscripts/ec2-creation.tf
@@ -2,7 +2,7 @@ provider "aws"{
 region = "ap-south-1"
 }
 
-resources "aws_instance" "AWSINSTANCE"{
+resource "aws_instance" "AWSINSTANCE"{
 ami = "ami-002f6e91abff6eb96"
 instance_type = "t2.micro"
 key_name = "javeed.pem"
